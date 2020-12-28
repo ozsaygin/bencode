0de79fddcadee280dc0ebd196764d106182e7404
Oğuz Özsaygın <oguzozsaygin@sabanciuniv.edu>
2020-12-28 22:48:31 +0300

    Finish bencode encoder and test with basis cases

--- bencode.go
+++ bencode.go
@@ -1,7 +1,8 @@
 package bencode
 
 import (
-	"regexp"
+	"fmt"
+	"strconv"
 )
 
 // Strings are length-prefixed base ten followed by a colon and the string. For example 4:spam corresponds to 'spam'.
@@ -13,40 +14,99 @@ import (
 // Dictionaries are encoded as a 'd' followed by a list of alternating keys and their corresponding values followed by an 'e'. For example, d3:cow3:moo4:spam4:eggse corresponds to {'cow': 'moo', 'spam': 'eggs'} and d4:spaml1:a1:bee corresponds to {'spam': ['a', 'b']}. Keys must be strings and appear in sorted order (sorted as raw strings, not alphanumerics).
 
 // Decode unmarshalls bencoded string to map object
-func Decode(data string) map[string]string {
+// func Decode(data string) map[string]string {
 
-	m := make(map[string]string)
+// 	strRegex = regexp.MustCompile(`de`)
+// 	intRegex = regexp.MustCompile()
+// 	lstRegex = regexp.MustCompile()
+// 	mapRegex = regexp.MustCompile()
 
-	// process dictionary
-	if data[0] == 'd' {
-		dataLen := len(data)
-		data = data[1 : dataLen-1]
+// 	// process dictionary
+// 	if data[0] == 'd' {
+// 		m := make(map[string]string)
+// 		dataLen := len(data)
+// 		data = data[1 : dataLen-1]
 
-		for len(data) > 1 {
-			// first element must be string
-			// hence, it must be in form of digit:word
+// 		for len(data) > 1 {
+// 			// first element must be string
+// 			// hence, it must be in form of digit:word
 
-			// first key comes and then value
-			// repeat the same process
-			re := regexp.MustCompile(`(\d+)\:([a-zA-Z]+)`)
-			matches := re.FindStringSubmatch(data)
-			var key string
-			if len(matches) > 1 {
-				key = matches[2]
-			}
-			data = data[len(matches[0]):]
-			// Process the value
-			// For now value can be only string
-			re = regexp.MustCompile(`(\d+)\:([a-zA-Z]+)`)
-			matches = re.FindStringSubmatch(data)
-			if len(matches) > 1 {
-				value := matches[2]
-				m[key] = value
-			}
-			data = data[len(matches[0]):]
+// 			// first key comes and then value
+// 			// repeat the same process
+// 			re := regexp.MustCompile(`(\d+)\:([a-zA-Z]+)`)
+// 			matches := re.FindStringSubmatch(data)
+// 			var key string
+// 			if len(matches) > 1 {
+// 				key = matches[2]
+// 			}
+// 			data = data[len(matches[0]):]
+
+// 			// Process the value
+// 			// For now value can be only string
+
+// 			if data[0] == 'i' {
+// 				// value is integer
+// 				re = regexp.MustCompile(`\i(\d+)\e`)
+// 				matches = re.FindStringSubmatch(data)
+// 				fmt.Println(matches)
+
+// 			} else {
+
+// 				re = regexp.MustCompile(`(\d+)\:([a-zA-Z]+)`)
+// 				matches = re.FindStringSubmatch(data)
+// 				if len(matches) > 1 {
+// 					value := matches[2]
+// 					m[key] = value
+// 				}
+// 				data = data[len(matches[1]):]
+// 			}
+// 			return m
+// 		}
+// 	}
+// }
+
+// Encode marshalls any input type into bencoded format
+func Encode(v interface{}) string {
+
+	switch v.(type) {
+
+	case string:
+
+		word := v.(string)
+		length := strconv.Itoa(len(word))
+		encoded := fmt.Sprintf("%s:%s", length, word)
+		return encoded
+
+	case int:
+
+		number := v.(int)
+		encoded := fmt.Sprintf("i%de", number)
+		return encoded
+
+	case []interface{}:
+
+		content := ""
+		for elm := range v.([]interface{}) {
+			content += Encode(elm)
 		}
+
+		encoded := fmt.Sprintf("l%se", content)
+		return encoded
+
+	case map[string]interface{}:
+
+		content := ""
+		for k, v := range v.(map[string]interface{}) {
+			content += fmt.Sprintf("%s%s", Encode(k), Encode(v))
+		}
+
+		encoded := fmt.Sprintf("d%se", content)
+		return encoded
+
+	default:
+		return ""
+
 	}
-	return m
 }
 
 // func main() {
--- bencode_test.go
+++ bencode_test.go
@@ -1,6 +1,7 @@
 package bencode
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -13,36 +14,97 @@ import (
 
 // Dictionaries are encoded as a 'd' followed by a list of alternating keys and their corresponding values followed by an 'e'. For example, d3:cow3:moo4:spam4:eggse corresponds to {'cow': 'moo', 'spam': 'eggs'} and d4:spaml1:a1:bee corresponds to {'spam': ['a', 'b']}. Keys must be strings and appear in sorted order (sorted as raw strings, not alphanumerics).
 
-func TestDecode(t *testing.T) {
+// func TestDecode(t *testing.T) {
+// 	type args struct {
+// 		data string
+// 	}
+// 	tests := []struct {
+// 		name string
+// 		args args
+// 		want map[string]interface{}
+// 	}{
+// 		{
+// 			name: "single string key value map",
+// 			args: args{"d3:cow3:mooe"},
+// 			want: map[string]interface{}{
+// 				"cow": "moo",
+// 			},
+// 		},
+// 		{
+// 			name: "multiple string key value map",
+// 			args: args{"d3:cat4:meow4:frog5:croak6:pigeon3:cooe"},
+// 			want: map[string]interface{}{
+// 				"cat":    "meow",
+// 				"frog":   "croak",
+// 				"pigeon": "coo",
+// 			},
+// 		},
+// 		{
+// 			name: "map containing single str int key value",
+// 			args: args{"d3:cati5ee"},
+// 			want: map[string]interface{}{
+// 				"cat": 5,
+// 			},
+// 		},
+// 		{
+// 			name: "map multiple str int key value",
+// 			args: args{"d3:cat4:frog2:11"},
+// 			want: map[string]interface{}{
+// 				"cat":    3,
+// 				"frog":   "11",
+// 				"pigeon": "coo",
+// 				"lion":   18,
+// 			},
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			if got := Decode(tt.args.data); !reflect.DeepEqual(got, tt.want) {
+// 				t.Errorf("Decode() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
+
+func TestEncode(t *testing.T) {
 	type args struct {
-		data string
+		v interface{}
 	}
 	tests := []struct {
 		name string
 		args args
-		want map[string]string
 	}{
 		{
-			name: "single string key value map",
-			args: args{"d3:cow3:mooe"},
-			want: map[string]string{
-				"cow": "moo",
-			},
+			name: "",
+			args: args{"spam"},
 		},
 		{
-			name: "multiple string key value map",
-			args: args{"d3:cat4:meow4:frog5:croak6:pigeon3:cooe"},
-			want: map[string]string{
-				"cat":    "meow",
-				"frog":   "croak",
-				"pigeon": "coo",
-			},
+			name: "",
+			args: args{"spam"},
 		},
+		{
+			name: "",
+			args: args{map[string]interface{}{
+				"name":     "Cat",
+				"genre":    "Scific",
+				"year":     2015,
+				"actrices": []string{"A", "B", "C"},
+				"maxLifeSpan": 16
+			}},
+		},
+		// {
+		// 	name: "simple string",
+		// 	args: args{[]string{"A", "B", "C"}},
+		// },
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Decode(tt.args.data); !reflect.DeepEqual(got, tt.want) {
+			result := Encode(tt.args.v)
+			fmt.Println(result)
+
+			if got := Encode(tt.args.data); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Decode() = %v, want %v", got, tt.want)
+
 			}
 		})
 	}
